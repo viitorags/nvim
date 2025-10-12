e86b6be481cf2b4b276278088608e810d9aaa303
viitorags <vitorgabriels1000@gmail.com>
2025-10-11 21:43:50 -0300

    change colorscheme to fluoromachine.nvim

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,10 +1,24 @@
 return {
   {
+    'maxmx03/fluoromachine.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local fm = require 'fluoromachine'
+      fm.setup {
+        glow = false,
+        theme = 'fluoromachine',
+        transparent = true,
+      }
+      vim.cmd.colorscheme 'fluoromachine'
+    end,
+  },
+  {
     'samharju/synthweave.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'synthweave'
+      -- vim.cmd.colorscheme 'synthweave'
     end,
   },
   {
