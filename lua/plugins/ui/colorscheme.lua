64b8478fb9c8e23706f0a491af205a192ec5c766
viitorags <vitorgabriels1000@gmail.com>
2025-10-17 19:51:49 -0300

    feat: adding smear-cursor
    - change colorscheme to gruvbox-material

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -10,15 +10,36 @@ return {
         theme = 'fluoromachine',
         transparent = true,
       }
-      vim.cmd.colorscheme 'fluoromachine'
+      -- vim.cmd.colorscheme 'fluoromachine'
     end,
   },
   {
-    'samharju/synthweave.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    'sainnhe/gruvbox-material',
+    lazy = false,
     priority = 1000,
     config = function()
-      -- vim.cmd.colorscheme 'synthweave'
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_foreground = 'mix'
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_float_style = 'bright'
+      vim.g.gruvbox_material_ui_contrast = 'high'
+
+      local grpid = vim.api.nvim_create_augroup('custom_highlights_gruvboxmaterial', {})
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        group = grpid,
+        pattern = 'gruvbox-material',
+        callback = function()
+          local function hl_lnk(src, trgt)
+            vim.api.nvim_set_hl(0, src, { link = trgt })
+          end
+
+          hl_lnk('@constant.php', 'Purple')
+          hl_lnk('@punctuation.bracket.php', 'Blue')
+        end,
+      })
+
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
   {
---
+++ lua/plugins/ui/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+  'sphamba/smear-cursor.nvim',
+  opts = {
+    stiffness = 0.5,
+    trailing_stiffness = 0.49,
+    never_draw_over_target = false,
+    legacy_computing_symbols_support = true,
+    smear_insert_mode = false,
+  },
+}
