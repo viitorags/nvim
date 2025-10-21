7878e98c34d8a8d838d7d5fd98e7058d0d129ee4
viitorags <vitorgabriels1000@gmail.com>
2025-10-21 16:35:04 -0300

    build(lint): disable phpcs linter for PHP files

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -12,7 +12,7 @@ return {
         bash = { 'shellcheck' },
         dockerfile = { 'hadolint' },
         go = { 'golangcilint' },
-        php = { 'phpcs' },
+        -- php = { 'phpcs' },
       }
 
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
