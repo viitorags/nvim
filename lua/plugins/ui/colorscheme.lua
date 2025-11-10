6fa9c455b855127f90066bdd43d1105a3af88311
viitorags <vitorgabriels1000@gmail.com>
2025-11-10 18:52:27 -0300

    feat(ui): update gruvbox_material foreground to 'material' and set background to 'soft'

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -6,7 +6,8 @@ return {
     config = function()
       vim.g.gruvbox_material_enable_italic = 1
       vim.g.gruvbox_material_enable_bold = 1
-      vim.g.gruvbox_material_foreground = 'original'
+      vim.g.gruvbox_material_foreground = 'material'
+      vim.g.gruvbox_material_background = 'soft'
       vim.g.gruvbox_material_better_performance = 1
       vim.g.gruvbox_material_float_style = 'bright'
       vim.g.gruvbox_material_ui_contrast = 'high'
