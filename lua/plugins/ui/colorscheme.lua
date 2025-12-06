235629b8b0918555c992470ad0840fb644324f57
viitorags <vitorgabriels1000@gmail.com>
2025-12-06 20:10:05 -0300

    feat(ui): replace gruvbox-material and kanagawa themes with onedarkpro

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,74 +1,36 @@
 return {
   {
-    'sainnhe/gruvbox-material',
-    lazy = false,
+    'olimorris/onedarkpro.nvim',
     priority = 1000,
     config = function()
-      if require('nixCatsUtils').isNixCats then
-        vim.g.gruvbox_material_transparent_background = 1
-      end
-      vim.g.gruvbox_material_enable_italic = 1
-      vim.g.gruvbox_material_enable_bold = 1
-      vim.g.gruvbox_material_foreground = 'original'
-      vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_float_style = 'bright'
-      vim.g.gruvbox_material_ui_contrast = 'high'
-
-      local grpid = vim.api.nvim_create_augroup('custom_highlights_gruvboxmaterial', {})
-      vim.api.nvim_create_autocmd('ColorScheme', {
-        group = grpid,
-        pattern = 'gruvbox-material',
-        callback = function()
-          local function hl_lnk(src, trgt)
-            vim.api.nvim_set_hl(0, src, { link = trgt })
-          end
-
-          hl_lnk('@constant.php', 'Purple')
-          hl_lnk('@punctuation.bracket.php', 'Blue')
-        end,
-      })
-
-      vim.cmd.colorscheme 'gruvbox-material'
-    end,
-  },
-  {
-    'rebelot/kanagawa.nvim',
-    priority = 1000,
-    config = function()
-      require('kanagawa').setup {
-        compile = false,
-        undercurl = true,
-        commentStyle = { italic = true },
-        functionStyle = { italic = true },
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = true,
-        dimInactive = false,
-        terminalColors = true,
-        colors = {
-          palette = {},
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = 'none',
-                float = {
-                  bg = 'none',
-                },
-              },
-            },
-          },
+      require('onedarkpro').setup {
+        options = {
+          cursorline = true,
+          highlight_inactive_windows = false,
         },
-        overrides = function()
-          return {}
-        end,
-        theme = 'dragon',
-        background = {
-          dark = 'dragon',
-          light = 'lotus',
+        highlights = {
+          LineNr = { bg = 'NONE' },
+          Comment = { italic = true },
+          Directory = { bold = true },
+          ErrorMsg = { italic = true, bold = true },
+        },
+        styles = {
+          types = 'NONE',
+          methods = 'NONE',
+          numbers = 'NONE',
+          strings = 'NONE',
+          comments = 'italic',
+          keywords = 'bold,italic',
+          constants = 'NONE',
+          functions = 'italic',
+          operators = 'NONE',
+          variables = 'NONE',
+          parameters = 'italic',
+          conditionals = 'italic',
+          virtual_text = 'NONE',
         },
       }
-      -- vim.cmd.colorscheme 'kanagawa-dragon'
+      vim.cmd.colorscheme 'vaporwave'
     end,
   },
 }
