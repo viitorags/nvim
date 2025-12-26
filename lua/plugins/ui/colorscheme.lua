1cb252fa0a27756319026b9b6316ea9a0754e712
viitorags <vitorgabriels1000@gmail.com>
2025-12-25 23:12:52 -0300

    feat(ui): change colorscheme

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,5 +1,37 @@
 return {
   {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      if require('nixCatsUtils').isNixCats then
+        vim.g.gruvbox_material_transparent_background = 1
+      end
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_foreground = 'original'
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_float_style = 'bright'
+      vim.g.gruvbox_material_ui_contrast = 'high'
+
+      local grpid = vim.api.nvim_create_augroup('custom_highlights_gruvboxmaterial', {})
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        group = grpid,
+        pattern = 'gruvbox-material',
+        callback = function()
+          local function hl_lnk(src, trgt)
+            vim.api.nvim_set_hl(0, src, { link = trgt })
+          end
+
+          hl_lnk('@constant.php', 'Purple')
+          hl_lnk('@punctuation.bracket.php', 'Blue')
+        end,
+      })
+
+      vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+  {
     'olimorris/onedarkpro.nvim',
     priority = 1000,
     config = function()
@@ -30,7 +62,7 @@ return {
           virtual_text = 'NONE',
         },
       }
-      vim.cmd.colorscheme 'vaporwave'
+      -- vim.cmd.colorscheme 'vaporwave'
     end,
   },
 }
