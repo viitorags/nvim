b96b4a107195da8a2ba91d828cdacf7d68259dc8
viitorags <vitorgabriels1000@gmail.com>
2026-01-16 00:01:23 -0300

    feat(ui): add gruvbox-material colorscheme with custom highlights

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,5 +1,52 @@
 return {
   {
+    'sainnhe/gruvbox-material',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      if require('nixCatsUtils').isNixCats then
+        vim.g.gruvbox_material_transparent_background = 1
+      end
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_foreground = 'original'
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_float_style = 'bright'
+      vim.g.gruvbox_material_ui_contrast = 'high'
+
+      local grpid = vim.api.nvim_create_augroup('custom_highlights_gruvboxmaterial', {})
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        group = grpid,
+        pattern = 'gruvbox-material',
+        callback = function()
+          local function hl_lnk(src, trgt)
+            vim.api.nvim_set_hl(0, src, { link = trgt })
+          end
+
+          local hl = vim.api.nvim_set_hl
+
+          hl(0, 'MyComment', { link = 'Comment', italic = true })
+          hl(0, 'MyKeyword', { link = 'Keyword', bold = true, italic = true })
+          hl(0, 'MyFunction', { link = 'Function', italic = true })
+          hl(0, 'MyDelimiter', { link = 'Delimiter', italic = true })
+          hl(0, 'MyOperator', { link = 'Operator', italic = true })
+
+          hl(0, '@comment', { link = 'MyComment' })
+          hl(0, '@keyword', { link = 'MyKeyword' })
+          hl(0, '@function', { link = 'MyFunction' })
+          hl(0, '@punctuation.bracket', { link = 'MyDelimiter' })
+          hl(0, '@operator', { link = 'MyOperator' })
+          hl(0, '@parameter', { link = 'MyFunction' })
+
+          hl_lnk('@constant.php', 'Purple')
+          hl_lnk('@punctuation.bracket.php', 'Blue')
+        end,
+      })
+
+      vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+  {
     'olimorris/onedarkpro.nvim',
     priority = 1000,
     config = function()
@@ -30,7 +77,7 @@ return {
           virtual_text = 'NONE',
         },
       }
-      vim.cmd.colorscheme 'vaporwave'
+      -- vim.cmd.colorscheme 'vaporwave'
     end,
   },
 }
